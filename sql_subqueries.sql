37da88ec97f30fb0f0c57d942bd348d77feb687e
Ricardo Castanheira <ricardocastanheira@gmail.com>
2025-10-20 16:36:15 +0100

    Update

--- sql_subqueries.sql
+++ sql_subqueries.sql
@@ -11,9 +11,9 @@ SELECT title FROM film as f
 WHERE f.length > (SELECT AVG(length) from film);
 
 -- 3. Use a subquery to display all actors who appear in the film "Alone Trip".
-SELECT a.first_name, a.last_name, f.title FROM actor as a
-JOIN film_actor as fa
-ON a.actor_id = fa.actor_id
-JOIN film as f
-ON fa.film_id = f.film_id
-HAVING f.title = "Alone Trip";
+SELECT a.first_name, a.last_name
+FROM actor AS a
+JOIN film_actor AS fa ON a.actor_id = fa.actor_id
+WHERE fa.film_id = (
+  SELECT film_id FROM film WHERE title = "Alone Trip"
+);
